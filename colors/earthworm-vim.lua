aada5b3fe5d24945b028a2272f86576a5d569ffb
JWW127 <19317273+JWW127@users.noreply.github.com>
2022-11-26 23:11:04 -0600

    color changes

--- colors/earthworm-vim.lua
+++ colors/earthworm-vim.lua
@@ -12,47 +12,42 @@ local link = function(target, group)
     vim.api.nvim_command('highlight! link ' .. target .. ' '.. group)
 end
 
-
-
 -- core colors
-local lightblue = '#3b94c8' 
-local mediumblue = '#373f7f'
-local navy = '#000167'
-local purple = '#9103ff' 
-local ultraviolet = '#000154' 
+local suitlightblue = '#3b94c8' 
+local suitdarkblue = '#0A0D1C'
+local galaxypurple = '#9103ff' 
+local xray = '#000154' 
 local darkpurp = '#09050d'
 local worm = '#d9b19a' 
 local mediumworm = '#cb835a'
 local darkworm = '#c0673b'
-local brownorange = '#b24600'
-local lightgray = '#474747' 
-local darkred = '#a40000'
-local brightred = '#b11f1f'
-local mediumyellow = '#d0ad0d'
-local brightyellow = '#fadf00'
-local lavender = '#be77ff'
-local whitegray = '#fefefe' 
+local gunred = '#a40000'
+local chickenyellow = '#d0ad0d'
+local shockyellow = '#fadf00'
+local spacelavender = '#be77ff'
+local cowwhite = '#fefefe' 
 local green = '#1a4c00'
 local snotgreen = '#a5a50d'
 local boogergreen = '#7ace29'
+local phlegm = '#818165'
 local suitwhite = '#ffffff'
 
-local Color0 = whitegray --comments
+local Color0 = cowwhite --comments
 local Color1 = mediumworm --icon colors
 local Color2 = suitwhite --error stuff
-local Color3 = brightyellow --local public
+local Color3 = shockyellow --local public
 local Color4 = worm --strings
-local Color5 = '#ffffff' --Operator
-local Color6 = suitwhite --function - highlight - link
+local Color5 = suitwhite --Operator
+local Color6 = cowwhite --function - highlight - link
 local Color7 = suitwhite --parens not funcitonin
 local Color8 = worm  --args
 local Color9 = '#0A0D1C' --background
-local Color10 = '#9593be'
+local Color10 = snotgreen
 local Color11 = '#0a0a1a' --cursorline / columns
 local Color12 = boogergreen --gutter number line
 local Color13 = '#c43f91' --tabline
 local Color14 = '#000000' --TabLine
-local Color15 = mediumblue --indent char
+local Color15 = phlegm --indent char
 --something
 
 highlight('Comment', nil, Color0, 'italic')
@@ -70,22 +65,22 @@ highlight('Type', nil, Color6, nil)
 highlight('parens', nil, Color7, nil)
 highlight('Identifier', nil, Color8, 'italic')
 highlight('Comment', nil, nil, 'italic')
-highlight('StatusLine', Color10, Color9, nil)
-highlight('WildMenu', Color9, Color10, nil)
-highlight('Pmenu', Color9, Color10, nil)
-highlight('PmenuSel', Color10, Color9, nil)
-highlight('PmenuThumb', Color9, Color10, nil)
-highlight('Normal', Color9, lightblue, 'bold')
-highlight('Visual', purple, ultraviolet, nil)
+highlight('WildMenu', darkpurp, Color10, nil)
+highlight('Pmenu', darkpurp, Color10, nil)
+highlight('PmenuSel', spacelavender, darkpurp, nil)
+highlight('PmenuThumb', darkpurp, Color10, nil)
+highlight('Normal', Color9, suitlightblue, 'bold')
+highlight('Visual', galaxypurple, xray, nil)
 highlight('CursorLine', Color11, nil, nil)
-highlight('ColorColumn', nil, nil, nil)
+highlight('ColorColumn', suitwhite, nil, nil)
 highlight('SignColumn', Color9, nil, nil)
 highlight('LineNr', nil, Color12, italic)
 highlight('TabLine', Color9, Color13, nil)
 highlight('TabLineSel', Color14, Color9, nil)
-highlight('TabLineFill', Color9, Color13, nil)
+highlight('TabLineFill', suitwhite, Color13, nil)
 highlight('TSPunctDelimiter', nil, Color10, nil)
 highlight('IndentBlanklineChar',nil, Color15, nil)
+highlight('Cursor', gunred, nil , 'italic')
 
 link('Conditional', 'Operator')
 link('TSParameterReference', 'TSParameter')
