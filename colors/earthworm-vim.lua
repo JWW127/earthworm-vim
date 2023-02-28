1702a74eab00f78b27e9cf1042015db5929bf342
JWW127 <19317273+JWW127@users.noreply.github.com>
2023-02-28 00:05:53 -0600

    deleted dup code, cleaned up some colors, tho outside of EJ theme, it help with contrash

--- colors/earthworm-vim.lua
+++ colors/earthworm-vim.lua
@@ -1,38 +1,35 @@
-vim.cmd[[highlight clear]]
+vim.cmd [[highlight clear]]
 
 local highlight = function(group, bg, fg, attr)
     fg = fg and 'guifg=' .. fg or ''
     bg = bg and 'guibg=' .. bg or ''
     attr = attr and 'gui=' .. attr or ''
 
-    vim.api.nvim_command('highlight ' .. group .. ' '.. fg .. ' ' .. bg .. ' '.. attr)
+    vim.api.nvim_command('highlight ' .. group .. ' ' .. fg .. ' ' .. bg .. ' ' .. attr)
 end
 
 local link = function(target, group)
-    vim.api.nvim_command('highlight! link ' .. target .. ' '.. group)
+    vim.api.nvim_command('highlight! link ' .. target .. ' ' .. group)
 end
 
 -- core colors
 local suitlightblue = '#3b94c8'
 local suitdarkblue = '#0A0D1C'
 local galaxypurple = '#9103ff'
-local xray = '#000154' 
+local xray = '#000154'
 local darkpurp = '#09050d'
 local worm = '#d9b19a'
 local mediumworm = '#cb835a'
-local darkworm = '#c0673b'
-local gunred = '#a40000'
-local chickenyellow = '#d0ad0d'
 local shockyellow = '#fadf00'
 local spacelavender = '#be77ff'
 local cowwhite = '#F3F4EE'
-local green = '#1a4c00'
 local snotgreen = '#a5a50d'
 local boogergreen = '#7ace29'
 local phlegm = '#818165'
-local suitwhite = '#ffffff'
+local suitwhite = '#9d9faf'
+local raygun = '#e64242'
 
-local Color0 = cowwhite --comments
+local Color0 = suitwhite --comments
 local Color1 = mediumworm --icon colors
 local Color2 = suitwhite --error stuff
 local Color3 = shockyellow --local public
@@ -40,7 +37,7 @@ local Color4 = worm --strings
 local Color5 = suitwhite --Operator
 local Color6 = cowwhite --function - highlight - link
 local Color7 = suitwhite --parens not funcitonin
-local Color8 = worm  --args
+local Color8 = raygun --args
 local Color9 = suitdarkblue --background
 local Color10 = snotgreen
 local Color11 = '#0a0a1a' --cursorline / columns
@@ -50,37 +47,46 @@ local Color14 = '#000000' --TabLine
 local Color15 = phlegm --indent char
 --something
 
-highlight('Comment', nil, Color0, 'italic')
+highlight('Comment', nil, Color0, nil)
 highlight('Constant', nil, Color1, nil)
 highlight('Error', nil, Color2, nil)
-highlight('Type', nil, Color3, nil)
 highlight('String', nil, Color4, nil)
 highlight('Keyword', nil, Color3, nil)
 highlight('Conditional', nil, Color3, nil)
 highlight('Repeat', nil, Color3, nil)
 highlight('Operator', nil, Color5, nil)
-highlight('Type', nil, Color6, nil)
 highlight('Function', nil, Color6, nil)
 highlight('Type', nil, Color6, nil)
 highlight('parens', nil, Color7, nil)
-highlight('Identifier', nil, Color8, 'italic')
-highlight('Comment', nil, nil, 'italic')
+highlight('Identifier', nil, Color8, nil)
 highlight('WildMenu', darkpurp, Color10, nil)
 highlight('Pmenu', darkpurp, Color10, nil)
 highlight('PmenuSel', spacelavender, darkpurp, nil)
 highlight('PmenuThumb', darkpurp, Color10, nil)
-highlight('Normal', Color9, suitlightblue, 'bold')
+highlight('Normal', Color9, suitlightblue, nil)
 highlight('Visual', galaxypurple, xray, nil)
 highlight('CursorLine', Color11, nil, nil)
 highlight('ColorColumn', suitwhite, nil, nil)
 highlight('SignColumn', Color9, nil, nil)
-highlight('LineNr', nil, Color12, 'italic')
+highlight('LineNr', nil, Color12, nil)
 highlight('TabLine', Color9, Color13, nil)
 highlight('TabLineSel', Color14, Color9, nil)
 highlight('TabLineFill', suitwhite, Color13, nil)
 highlight('TSPunctDelimiter', nil, Color10, nil)
-highlight('IndentBlanklineChar',nil, Color15, nil)
-highlight('Cursor', boogergreen, nil , 'italic')
+highlight('IndentBlanklineChar', nil, Color15, nil)
+highlight('Cursor', boogergreen, nil, nil)
+
+highlight('Directory', nil, Color12, nil)
+-- unusual ones tuned to my prefs
+highlight('lspInlayHintsType', nil, Color0, nil)
+highlight('lspInlayHintsParameter', nil, Color0, nil)
+highlight('NvimInternalError', Color14, Color2, nil)
+highlight('NERDTreeExecFile', nil, '#fb4934', nil)
+highlight('TelescopeResultsBorder', nil, Color5, nil)
+highlight('TelescopePreviewBorder', nil, Color5, nil)
+highlight('TelescopePromptBorder', nil, Color5, nil)
+
+
 
 link('Conditional', 'Operator')
 link('TSParameterReference', 'TSParameter')
